529b5729adde5ac5de1f21c4ecf51de128e715aa
Davi Alves <davialealves@gmail.com>
2025-09-09 00:39:09 -0300

    UserController class

--- certifai/src/main/java/com/certifai/user/gateway/presentation/http/UserController.java
+++ certifai/src/main/java/com/certifai/user/gateway/presentation/http/UserController.java
@@ -1,4 +1,28 @@
 package com.certifai.user.gateway.presentation.http;
 
+import com.certifai.user.application.port.in.RegisterUser;
+import com.certifai.user.application.service.mapper.InputMapper;
+import com.certifai.user.gateway.presentation.http.resource.UserRequestBody;
+import com.certifai.user.gateway.presentation.http.resource.UserResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    private final RegisterUser registerUser;
+    private final InputMapper inputMapper;
+
+    public UserController(RegisterUser registerUser, InputMapper inputMapper){
+        this.registerUser = registerUser;
+        this.inputMapper = inputMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResource> register(@RequestBody UserRequestBody userRequestBody){
+        final var user = registerUser.execute(inputMapper.toDomain(userRequestBody));
+        final var userResource = inputMapper.toResource(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResource);
+    }
 }

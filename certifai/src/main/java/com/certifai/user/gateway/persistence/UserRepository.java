d69b877fec6b87a4a0ba052f327134aab10e870f
Davi Alves <davialealves@gmail.com>
2025-09-08 21:40:31 -0300

    UserEntity implementation

--- certifai/src/main/java/com/certifai/user/gateway/persistence/UserRepository.java
+++
@@ -1,4 +0,0 @@
-package com.certifai.user.gateway.persistence;
-
-public class UserRepository {
-}
---
+++ certifai/src/main/java/com/certifai/user/gateway/persistence/jpa/UserEntity.java
@@ -0,0 +1,63 @@
+package com.certifai.user.gateway.persistence.jpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name="tb_user")
+public class UserEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UserEntity that = (UserEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}

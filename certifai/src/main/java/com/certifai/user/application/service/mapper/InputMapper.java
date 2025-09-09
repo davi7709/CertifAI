0b7a17a20957e3d6014743a5e4dbd8d0a88c618b
Davi Alves <davialealves@gmail.com>
2025-09-09 00:41:12 -0300

    InputMapper interface

--- certifai/src/main/java/com/certifai/user/application/service/mapper/InputMapper.java
+++ certifai/src/main/java/com/certifai/user/application/service/mapper/InputMapper.java
@@ -1,5 +1,13 @@
 package com.certifai.user.application.service.mapper;
 
+import com.certifai.user.domain.User;
+import com.certifai.user.gateway.presentation.http.resource.UserRequestBody;
+import com.certifai.user.gateway.presentation.http.resource.UserResource;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface InputMapper {
+    User toDomain(UserRequestBody userRequestBody);
+    UserResource toResource(User user);
 }

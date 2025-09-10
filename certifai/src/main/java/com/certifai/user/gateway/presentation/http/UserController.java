611118ba05483c429baa2fc5b7e4c4f465b834f7
FelipeCondutta <164044858+FelipeCondutta@users.noreply.github.com>
2025-09-10 17:03:27 -0300

    user email and password validation

--- certifai/src/main/java/com/certifai/user/gateway/presentation/http/UserController.java
+++ certifai/src/main/java/com/certifai/user/gateway/presentation/http/UserController.java
@@ -4,6 +4,7 @@ import com.certifai.user.application.port.in.RegisterUser;
 import com.certifai.user.application.service.mapper.InputMapper;
 import com.certifai.user.gateway.presentation.http.resource.UserRequestBody;
 import com.certifai.user.gateway.presentation.http.resource.UserResource;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserResource> register(@RequestBody UserRequestBody userRequestBody){
+    public ResponseEntity<UserResource> register(@Valid @RequestBody UserRequestBody userRequestBody){
         final var user = registerUser.execute(inputMapper.toDomain(userRequestBody));
         final var userResource = inputMapper.toResource(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(userResource);
--- certifai/src/main/java/com/certifai/user/gateway/presentation/http/resource/UserRequestBody.java
+++ certifai/src/main/java/com/certifai/user/gateway/presentation/http/resource/UserRequestBody.java
@@ -1,4 +1,23 @@
 package com.certifai.user.gateway.presentation.http.resource;
 
-public record UserRequestBody(String name, String email, String password) {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UserRequestBody(
+        @NotBlank
+        String name,
+
+        @NotBlank
+        @Email(message = "Email inválido")
+        String email,
+
+        @NotBlank
+        @Size(min = 5, message = "A senha deve ter no mínimo 5 caracteres")
+        @Pattern(
+                regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}:;<>.,]).{5,}$",
+                message = "A senha deve conter ao menos uma letra maiúscula, um número e um caractere especial"
+        )
+        String password
+) {}

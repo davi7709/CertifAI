6e0429417c662688e5dfab8ade7d3e13707e3dfd
unknown <davialealves@gmail.com>
2025-09-11 23:46:37 -0300

    Package exception

--- certifai/src/main/java/com/certifai/user/gateway/presentation/http/exception/ValidationExceptionHandler.java
+++ certifai/src/main/java/com/certifai/user/gateway/presentation/http/exception/ValidationExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.certifai.user.gateway.presentation.http;
+package com.certifai.user.gateway.presentation.http.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
